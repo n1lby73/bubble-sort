0a8d737db48cc178aba50c4a3583f38e567b9c8a
n1lby73 <n1lby73@gmail.com>
2023-01-11 20:46:43 +0100

    added a test file to fix endless loop when user enters a string

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int b;
+int main(){
+
+    b = 7 * 1;
+
+    printf("%d", b);
+
+    // printf("try input a number");
+
+    // while (scanf("%d", &b) != 1){
+    //     printf ("wrong");
+    //     break;
+    // }
+}
